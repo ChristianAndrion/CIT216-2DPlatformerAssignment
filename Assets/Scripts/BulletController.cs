745bfacc2185e033221ee5e7936970fd931a0b46
ChristianAndrion <cfa3@pct.edu>
2026-01-29 10:46:33 -0500

    Initial Commit, I forgort to do this earlier

---
+++ Assets/Scripts/BulletController.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float bulletForce = 300f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        Vector2 direction = player.GetComponent<PlayerController>().GetDirection();
+
+        rb = GetComponent<Rigidbody2D>();
+        rb.AddForce(Vector2.right * bulletForce);
+        rb.linearVelocity = direction * bulletForce;
+        Invoke("Die", 3f);
+    }
+
+    void Die()
+    {
+        Destroy(gameObject); //Dont use "this" as it refers to the component/script
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
